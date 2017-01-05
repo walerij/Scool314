ab99f24bbb8ff4e0513aba0a971a88244d6bbb29
walera <becwal@yandex.ru>
2017-01-05 18:18:21 +0300

    создание проекта, перенос файлов

---
+++ www/index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Видеошкола 3.14</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+
+<div class="container">
+    <div class="row">
+        <div class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <div class="navbar navbar-header">
+                    <button class="navbar-toggle" data-toggle="collapse" data-target="#MyTopMenu" >
+                        <span class="sr-only">Открыть меню</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse" id="MyTopMenu">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#" id="logo" title="Видеошкола 3.14">
+                                <img src="images/fmap.png" style="width: 30px; height: 30px;" alt="fmzp">
+                            </a></li>
+                        <li><a href="#">Начало</a></li>
+                        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Курсы<b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">C#</a></li>
+                                <li><a href="#">Java</a></li>
+                                <li><a href="#">ASP.NET</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Вебинары<b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Расписание</a></li>
+                                <li><a href="#">Подключение</a></li>
+                                <li><a href="#">Архив</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Консоль</a></li>
+                        <li><a href="#">Магазин</a></li>
+
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="https://www.videosharp.info/users/1/avatar/1_avatar_1.jpg" class="img-circle" style="width: 30px; height: 30px;">&nbsp;&nbsp;Игромистр <b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Панель управления</a></li>
+                                <li><a href="#">Управление ролями пользователей</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">Выход</a></li>
+                            </ul>
+
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <h2>&nbsp;</h2>
+        <h2>&nbsp;</h2>
+    </div>
+    <div class="row">
+        <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3">
+            <a href="#" title="Простые игры начального уровня">
+                <div class="thumbnail">
+                    <image src="images/demo.png" class="image-butt-cl">
+                        <div class="caption" style="text-align: center">
+                            <h4>Демо игры</h4>
+                        </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3">
+            <a href="#" title="Создание игры Сокобан">
+                <div class="thumbnail">
+                    <image src="images/soko.png" class="image-butt-cl">
+                        <div class="caption" style="text-align: center">
+                            <h4>Сокобан</h4>
+                        </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3">
+            <a href="#" title="Изучение теоретических основ ООП">
+                <div class="thumbnail">
+                    <image src="images/oop.png" class="image-butt-cl">
+                        <div class="caption" style="text-align: center">
+                            <h4>Теория ООП</h4>
+                        </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3">
+            <a href="#" title="Создание игры в шахматы на HTML/JS">
+                <div class="thumbnail">
+                    <image src="images/shah.png" class="image-butt-cl">
+                        <div class="caption" style="text-align: center">
+                            <h4>Нано Шахматы</h4>
+                        </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3">
+            <a href="#" title="Технологии C# и PHP">
+                <div class="thumbnail">
+                    <image src="images/ship3.png" class="image-butt-cl">
+                        <div class="caption" style="text-align: center">
+                            <h4>Морской флот</h4>
+                        </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3">
+            <a href="#" title="Создание игры Xonix на языке Java">
+                <div class="thumbnail">
+                    <image src="images/xonix.png" class="image-butt-cl">
+                        <div class="caption" style="text-align: center">
+                            <h4>Java Xonix</h4>
+                        </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3">
+            <a href="#" title="Создание игры Сапер">
+                <div class="thumbnail">
+                    <image src="images/mine2.png" class="image-butt-cl">
+                        <div class="caption" style="text-align: center">
+                            <h4>Сапер</h4>
+                        </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3">
+            <a href="#" title="Изучение ООП в Java в новогодней игре">
+                <div class="thumbnail">
+                    <image src="images/fruits2.png" class="image-butt-cl">
+                        <div class="caption" style="text-align: center">
+                            <h4>ООФрукты</h4>
+                        </div>
+                </div>
+            </a>
+        </div>
+
+    </div>
+</div>
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
