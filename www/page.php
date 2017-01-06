1079536ccf71dd5fd76f9ba58f576346d87426e7
walera <becwal@yandex.ru>
2017-01-06 16:41:21 +0300

    Добавление странички с подробностями курса

---
+++ www/page.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Видеошкола 3.14-курс</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+ <div class="container">
+    <div class="row">
+        <div class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <div class="navbar navbar-header">
+                    <button class="navbar-toggle" data-toggle="collapse" data-target="#MyTopMenu" >
+                        <span class="sr-only">Открыть меню</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse" id="MyTopMenu">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#" id="logo" title="Видеошкола 3.14">
+                                <img src="images/fmap.png" style="width: 30px; height: 30px;" alt="fmzp">
+                            </a></li>
+                        <li><a href="#">Начало</a></li>
+                        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Курсы<b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">C#</a></li>
+                                <li><a href="#">Java</a></li>
+                                <li><a href="#">ASP.NET</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Вебинары<b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Расписание</a></li>
+                                <li><a href="#">Подключение</a></li>
+                                <li><a href="#">Архив</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Консоль</a></li>
+                        <li><a href="#">Магазин</a></li>
+
+                    </ul>
+                    <!-- <ul class="nav navbar-nav navbar-right">
+                         <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="https://www.videosharp.info/users/1/avatar/1_avatar_1.jpg" class="img-circle" style="width: 30px; height: 30px;">&nbsp;&nbsp;Игромистр <b class="caret"></b></a>
+                             <ul class="dropdown-menu">
+                                 <li><a href="#">Панель управления</a></li>
+                                 <li><a href="#">Управление ролями пользователей</a></li>
+                                 <li class="divider"></li>
+                                 <li><a href="#">Выход</a></li>
+                             </ul>
+
+                         </li>
+                     </ul>-->
+                </div>
+            </div>
+        </div>
+    </div>
+ </div>
+
+
+</body>
+</html>
